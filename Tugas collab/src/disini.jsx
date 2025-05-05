67f53eb670a39a587346e77954259dbf4d182d68
imagham <151702624+imagham@users.noreply.github.com>
2025-05-05 09:53:17 +0700

    g

---
+++ Tugas collab/src/disini.jsx
@@ -0,0 +1,12 @@
+import fotofadgham from './assets/fotofadgham.jpg'
+export default function App() {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold ">
+      Fadgham Khairul Hafizh
+    </h1>
+    <img src={fotofadgham} alt="" />
+    </div>
+    
+  )
+}
