e07fc3df665dae62bb6c3527588ef17afbf906ef
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2025-05-05 09:57:46 +0700

    hammam

--- Tugas collab/src/disini.jsx
+++ Tugas collab/src/disini.jsx
@@ -1,12 +1,13 @@
-import fotofadgham from './assets/fotofadgham.jpg'
+import fotofadgham from "./assets/fotofadgham.jpg";
+import fotofathan from "./assets/npcbot.jpg";
 export default function App() {
   return (
     <div>
-    <h1 className="text-3xl font-bold ">
-      Fadgham Khairul Hafizh
-    </h1>
-    <img src={fotofadgham} alt="" />
+      <h1 className="text-3xl font-bold ">Fadgham Khairul Hafizh</h1>
+      <img src={fotofadgham} alt="" />
+      <h1 className="text-3xl font-bold ">Jusuf fathan Nuradly</h1>
+      <p>ini foto mas kim ham mam viktor john ferdy ganteng gila ggs</p>
+      <img src={fotofathan} alt="" />
     </div>
-    
-  )
+  );
 }
