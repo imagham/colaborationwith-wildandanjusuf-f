fd5519a39bd491535b58c56f1c4043f75269b873
WildanCool <wildan2512@student.abudzar.sch.id>
2025-05-05 10:04:41 +0700

    Wildan Nambahin Foto

--- Tugas collab/src/disini.jsx
+++ Tugas collab/src/disini.jsx
@@ -1,13 +1,19 @@
 import fotofadgham from "./assets/fotofadgham.jpg";
-import fotofathan from "./assets/npcbot.jpg";
+import fotofathan from "./assets/npcbot.png";
+import fotoWildan from "./assets/welldone.jpg"
 export default function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold ">Fadgham Khairul Hafizh</h1>
       <img src={fotofadgham} alt="" />
+      
       <h1 className="text-3xl font-bold ">Jusuf fathan Nuradly</h1>
       <p>ini foto mas kim ham mam viktor john ferdy ganteng gila ggs</p>
       <img src={fotofathan} alt="" />
+
+      <h1 className="text-3xl font-bold ">Perdana Muhammad Wildanumukhaladun</h1>
+      <p>Infokan Gym Terdekatt!</p>
+      <img src={fotoWildan} alt="" />
     </div>
   );
 }
